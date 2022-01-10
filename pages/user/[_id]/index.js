9f941fa45cd5da178235d8bc6d96649563d3a034
AsP3X <zombymedia@gmail.com>
2022-01-10 17:50:14 +0100

    Revert "Revert "FIX: datafetch error when api block is present""
    This reverts commit 3e2820c62497cc269a13cb8f9cdb9107a701aa5a.

--- pages/user/[_id]/index.js
+++ pages/user/[_id]/index.js
@@ -2,33 +2,50 @@ import React from 'react'
 
 
 const User = ({ user }) => {
-  return (
-    <div>
-      <h3>{user.username}</h3>
-      <p>UserID: {user._id} </p>
-      <p>Email: {user.email} </p>
-      <p>Created: {user.createdAt} </p>
-    </div>
-  )
+  if (user) {
+    return (
+      <div>
+        <h3>{user.username}</h3>
+        <p>UserID: {user._id} </p>
+        <p>Email: {user.email} </p>
+        <p>Created: {user.createdAt} </p>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <p>API server is unresponsive</p>
+      </div>
+    )
+  }
 }
 
 export async function getStaticPaths() {
-  const res = await fetch('http://localhost:3030/users')
-  const users = await res.json()
+  const res = await fetch('http://localhost:3000/users')
 
-  const paths = users.map((user) => ({
-    params: { _id: user._id.toString() },
-  }))
+  if (res.ok) {
+    const users = await res.json()
 
-  return { paths, fallback: false }
+    const paths = users.map((user) => ({
+      params: { _id: user._id.toString() },
+    }))
+
+    return { paths, fallback: false }
+  } else {
+    return { paths: [], fallback: false }
+  }
 }
 
 
 export async function getStaticProps({ params }) {
-  const res = await fetch(`http://localhost:3030/users/${params._id}`)
-  const user = await res.json()
+  if (params) {
+    const res = await fetch(`http://localhost:3000/users/${params._id}`)
+    const user = await res.json()
 
-  return { props: { user } }
+    return { props: { user } }
+  } else {
+    return { props: { user: null } }
+  }
 }
 
 export default User
